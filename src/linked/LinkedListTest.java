3184216962edaa73e4c51b9e33b1aa50be7d2b26
Charles Fei <charles.fei@vanderbilt.edu>
2015-06-26 15:22:22 -0500

    Added test class

---
+++ src/linked/LinkedListTest.java
@@ -0,0 +1,36 @@
+package linked;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by churkle on 6/26/15.
+ */
+public class LinkedListTest
+{
+
+    @Test
+    public void testAddItem() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testGetFirst() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testGetNext() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testGetPrev() throws Exception
+    {
+
+    }
+}
